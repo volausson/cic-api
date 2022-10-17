e178ad5366479ef886f847c72243d9fed5c2db5d
volausson <veronicagolausson@gmail.com>
2022-10-17 16:45:40 +0000

    add img filters and create postlist views

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,16 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
 
+    image_filter_choices = [
+        ('_1977', '1977'), ('brannan', 'Brannan'),
+        ('earlybird', 'Earlybird'), ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'), ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'), ('normal', 'Normal'),
+        ('nashville', 'Nashville'), ('rise', 'Rise'),
+        ('toaster', 'Toaster'), ('valencia', 'Valencia'),
+        ('walden', 'Walden'), ('xpro2', 'X-pro II')
+    ]
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -11,6 +21,10 @@ class Post(models.Model):
     content = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_post_pusmxh, blank=True'
+    )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices,
+        default='normal'
     )
 
     class Meta:
