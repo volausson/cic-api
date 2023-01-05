935f2e2bb3d5dc98c45ce54c4373cfaa34b9cffd
volausson <veronicagolausson@gmail.com>
2023-01-05 21:38:37 +0000

    made changes to settings.py to prepare for react

--- cic_api/settings.py
+++ cic_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import re
 from pathlib import Path
 import os
 import dj_database_url
@@ -65,18 +65,19 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    'cic-api.herokuapp.com', 'localhost',
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
 ]
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENTS_ORIGIN')
     ]
-else:
-    CORS_ALLOWED_ORIGINS_REGEXES = [
-        r'^https://.*\.gitpod\.io$',
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
-
 CORS_ALLOW_CREDENTIALS = True
 
 
