79e45ae6c0a734052e045a0bf1cb4bc83ef87193
volausson <veronicagolausson@gmail.com>
2023-01-06 18:55:26 +0000

    made minor changes in settings.py

--- cic_api/settings.py
+++ cic_api/settings.py
@@ -65,8 +65,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True    # 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    os.environ.get('ALLOWED_HOST'),
-    'localhost',
+    'localhost', 'cic_api.herokuapp.com'
 ]
 
 
